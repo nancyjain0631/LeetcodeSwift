75cb873febd7db858dbb1b97a106db35def87bd4
Nancy Jain <56497922+nancyjain0631@users.noreply.github.com>
2022-10-03 19:34:57 +0530

    Create 9. Palindrome Number.swift

---
+++ Easy/9. Palindrome Number.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func isPalindrome(_ x: Int) -> Bool {
+//         Idea is to reverse the number then compare x with reveredNum
+         x < 0 ? false : checkPalindrome(x) == x
+    }
+    
+    func checkPalindrome(_ x: Int) -> Int {
+        var reversedNum: Int
+        var rem: Int = 0
+        var copyX = x
+        while(copyX != 0){
+            rem = rem * 10
+            rem = rem + (copyX % 10)
+            copyX = copyX / 10
+        }
+        return rem
+    }  
+}
